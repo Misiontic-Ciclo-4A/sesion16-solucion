c9b7696dd3079871d2e211653a967c60141ff031
kristellu <kristellu@uninorte.edu.co>
2021-12-13 22:48:06 -0500

    Added solution correction

--- README.md
+++
@@ -1,3 +0,0 @@
-# sesion16-solucion
-
-Examples of how to monitor Node.js server applications
--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// const expressStatusMonitor = require('express-status-monitor');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const expressStatusmonitor = require("express-status-monitor");
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+// config
 const config = {
-    healthChecks: [{
-        protocol: 'http',
-        host: 'localhost',
-        path: '/',
-        port: '3000'
-    }, {
-        protocol: 'http',
-        host: 'localhost',
-        path: '/users',
-        port: '3000'
-    }]
-}
-// app.use(expressStatusMonitor());
+  healthChecks: [
+    {
+      protocol: "http",
+      host: "localhost",
+      path: "/",
+      port: "3000",
+    },
+    {
+      protocol: "http",
+      host: "localhost",
+      path: "/users",
+      port: "3000",
+    },
+  ],
+};
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(expressStatusmonitor());
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+  console.log("Conectado al puerto 3000");
+});
